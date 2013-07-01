fe33ab5838d85a0c625dd85146c05b2866641771
unknown <amuramatsu@LETS-CFB10-AKI.(none)>
2013-07-02 08:28:06 +0900

    mixpanel testing .php files

---
+++ dashboard/mixpanel_test.php
@@ -0,0 +1,19 @@
+<?php
+require 'mixpanel.php';
+$api_key = '4c0e9c992953d876ebd57c936e0e7be7';
+$api_secret = '5514393a22a43ba7437c9ccc385bc0d8';
+
+$mp = new Mixpanel($api_key, $api_secret);
+$data = $mp->request(array('events', 'properties'), array(
+    'event' => 'launch study app',
+    'name' => 'page',
+    'type' => 'general',
+    'unit' => 'month',
+    'interval' => '20',
+    'limit' => '20'
+));
+
+var_dump($data);
+
+
+?>
