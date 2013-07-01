3cdf659eaea5b0c35df3914ea25e526cdc41bda9
unknown <amuramatsu@LETS-CFB10-AKI.(none)>
2013-07-02 08:36:25 +0900

    correct parameters

--- dashboard/mixpanel_test.php
+++ dashboard/mixpanel_test.php
@@ -4,9 +4,8 @@ $api_key = '4c0e9c992953d876ebd57c936e0e7be7';
 $api_secret = '5514393a22a43ba7437c9ccc385bc0d8';
 
 $mp = new Mixpanel($api_key, $api_secret);
-$data = $mp->request(array('events', 'properties'), array(
+$data = $mp->request(array('events'), array(
     'event' => 'launch study app',
-    'name' => 'page',
     'type' => 'general',
     'unit' => 'month',
     'interval' => '12',
