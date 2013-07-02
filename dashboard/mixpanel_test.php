89c8582a92bcb47c49a67dceff7e15e71009eba9
unknown <amuramatsu@LETS-CFB10-AKI.(none)>
2013-07-03 06:30:27 +0900

    added google chart libs

--- dashboard/mixpanel_test.php
+++ dashboard/mixpanel_test.php
@@ -92,6 +92,44 @@ body {
 	border-radius: 10px;
 }
 		</style>
+		
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>		
+		
 	</head>
 <body>
 	
@@ -112,6 +150,17 @@ body {
           <h3>Study Completed</h3>
         </div>
       </div>
+      
+      <div class="row">
+        <div class="span4 blackbox">
+        	<div id="chart_div"></div>
+        </div>
+        <div class="span4 blackbox">
+       </div>
+        <div class="span4 blackbox">
+        </div>
+      </div>
+      
 
       <hr>
 
